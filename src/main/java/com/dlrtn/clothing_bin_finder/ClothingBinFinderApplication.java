b23bade9994b6a27053ed6db5b45510c8eae4f37
JooIcksoo <dlrtn751@gmail.com>
2025-04-10 21:06:26 +0900

    Initial commit

---
+++ src/main/java/com/dlrtn/clothing_bin_finder/ClothingBinFinderApplication.java
@@ -0,0 +1,13 @@
+package com.dlrtn.clothing_bin_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingBinFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingBinFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlrtn/clothing_bin_finder/ClothingBinFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlrtn.clothing_bin_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingBinFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
